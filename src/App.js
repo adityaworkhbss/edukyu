c732138627173d5458efa8f07b82241896c69f73
Sahilkumarbt <117939412+Sahilkumarbt@users.noreply.github.com>
2025-04-21 10:03:47 +0530

    footer added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import './App.css';
 import Navbar from "./navbar/Navbar";
 import ScholarshipGrantCard from "./FloatingSideCards/Component/ScholarshipGrantFloatingIcon";
 import FloatingSideCards from "./FloatingSideCards/FloatingSideCards";
+import Footer from "./Footer/Footer";
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-        <FloatingSideCards/>
+      {/*<Navbar/>*/}
+      {/*  <FloatingSideCards/>*/}
+        <Footer/>
+
     </div>
   );
 }
